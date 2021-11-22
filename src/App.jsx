f52186a2d904d3955a4ad49d75503cac6f9b2d42
pedrodruviaro <pedrodruviaro@gmail.com>
2021-11-22 15:54:19 -0300

    estruturando projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const App = () => {
+    return (
+        <>
+            <h1>React app</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+                non.
+            </p>
+            <a href="/">Link 01</a>
+            <button>Primary</button>
+        </>
+    );
+};
